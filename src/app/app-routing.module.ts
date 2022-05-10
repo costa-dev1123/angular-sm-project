e5072eb2d6213bb08e0ede99bc2d47cfae049c68
Costantino <csorrentino@sysmanagement.it>
2022-05-10 09:24:03 +0200

    added authGuard canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, 
   { path: 'catalog', loadChildren: () => import('./features/catalog/catalog.module').then(m => m.CatalogModule) },
-  { path: 'contacts', loadChildren: () => import('./features/contacts/contacts.module').then(m => m.ContactsModule) },
+  { 
+    path: 'contacts', 
+    loadChildren: () => import('./features/contacts/contacts.module').then(m => m.ContactsModule),
+    canActivate: [AuthGuard]
+  },
   { path: '**', redirectTo: 'home'}
 ];
 
